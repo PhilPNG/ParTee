93796de31a5b7c68aa1aeced405f58ab5b5b329c
PhilPNG <philiprrodriguez@gmail.com>
2025-08-09 08:21:09 -0400

    fix(frontend): ensure react-native-worklets babel plugin

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', 'react-native-worklets/plugin'],
+    plugins: [
+      'nativewind/babel',
+      // React Native Worklets plugin must come last
+      require.resolve('react-native-worklets/plugin'),
+    ],
   };
 };
